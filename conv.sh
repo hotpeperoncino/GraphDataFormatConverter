7991fc5304dafb0e824d29a93869a094a9df36dd
hotpeperoncino <bluenemophila@gmail.com>
2017-08-15 12:18:41 +0900

    conv.sh is changed to 32G

--- conv.sh
+++ conv.sh
@@ -1,2 +1,3 @@
-node app.js -f adj -t edge adj.txt output.txt
-cat output.txt
+NODE="node --max-old-space-size=32192 --optimize-for-size --max-executable-size=32192  --max_old_space_size=32192 --optimize_for_size --max_executable_size=32192"
+
+$NODE app.js -f adj -t edge kron_g500-logn20.graph output.txt
